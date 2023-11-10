fa545d56e01ab900166dd65fbb6fe26ba4a44676
siddharthaDevineni <siddhartha.devineni@gmail.com>
2023-11-10 16:43:05 +0100

    logging and two-way transfer test

--- test/SID.ts
+++ test/SID.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 
 describe("SID", function () {
   it("transfers tokens correctly", async function () {
@@ -14,7 +14,29 @@ describe("SID", function () {
       sidToken,
       [alice, bob],
       // differences in the balances between before and after transfer
-      [ethers.parseUnits("-10", 18), ethers.parseUnits("10", 18)]
+      [ethers.parseUnits("-10", 18), ethers.parseUnits("9", 18)]
+    );
+
+    console.log(
+      "Current balance of Alice: ",
+      (await sidToken.balanceOf(alice)).toString()
+    );
+    console.log(
+      "Current balance of Bob  : ",
+      (await sidToken.balanceOf(bob)).toString()
+    );
+    console.log(
+      "Current balance of token contract  : ",
+      (await sidToken.contractBalance()).toString()
+    );
+
+    await expect(
+      await sidToken.connect(bob).transfer(alice, ethers.parseUnits("5", 18))
+    ).to.changeTokenBalances(
+      sidToken,
+      [alice, bob],
+      // differences in the balances between before and after transfer
+      [ethers.parseUnits("45", 17), ethers.parseUnits("-5", 18)]
     );
   });
 });
