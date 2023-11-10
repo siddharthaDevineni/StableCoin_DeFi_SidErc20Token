e472be218ae104d1ce036a2a5161c8c18b288dbf
siddharthaDevineni <siddhartha.devineni@gmail.com>
2023-11-10 23:06:54 +0100

    unit tests using loadFixtures hardhat

--- test/SID.ts
+++ test/SID.ts
@@ -1,13 +1,17 @@
 import { expect, should } from "chai";
-import { ethers, network } from "hardhat";
+import { ethers } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 
 describe("SID", function () {
-  it("should transfer tokens correctly", async function () {
+  async function deploySidTokenERC20() {
     const [alice, bob] = await ethers.getSigners();
 
     const SID = await ethers.getContractFactory("SID");
     const sidToken = await SID.deploy();
-
+    return { alice, bob, sidToken };
+  }
+  it("should transfer tokens correctly", async function () {
+    const { alice, bob, sidToken } = await loadFixture(deploySidTokenERC20);
     console.log(
       "Balance of Alice after minting 100 tokens to her: ",
       ethers.formatEther(await sidToken.balanceOf(alice))
@@ -67,20 +71,14 @@ describe("SID", function () {
   });
 
   it("should revert on insufficient balance", async () => {
-    const [alice, bob] = await ethers.getSigners();
-
-    const SID = await ethers.getContractFactory("SID");
-    const sidToken = await SID.deploy();
+    const { alice, bob, sidToken } = await loadFixture(deploySidTokenERC20);
 
     await expect(sidToken.transfer(bob, ethers.parseUnits("200", 18))).to.be
       .reverted;
   });
 
   it("should deposit to the caller's account", async () => {
-    const [alice, bob] = await ethers.getSigners();
-
-    const SID = await ethers.getContractFactory("SID");
-    const sidToken = await SID.deploy();
+    const { alice, bob, sidToken } = await loadFixture(deploySidTokenERC20);
 
     console.log(
       "Balance of Alice before her deposit: ",
