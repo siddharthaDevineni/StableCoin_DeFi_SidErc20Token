5393f882d4fdb422a595474818417c5470ec3817
siddharthaDevineni <siddhartha.devineni@gmail.com>
2023-11-18 15:52:29 +0100

    hardhat-foundry

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-foundry";
 import { version } from "chai";
 
 const config: HardhatUserConfig = {
